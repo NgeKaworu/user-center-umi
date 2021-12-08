1f051b8e006a5d4b5cbdf2d79506182d21a7e6fe
NgeKaworu <NgeKaworu@163.com>
2021-12-08 16:23:44 +0800

    Squashed 'src/js-sdk/' content from commit d418042
    git-subtree-dir: src/js-sdk
    git-subtree-split: d4180421d14cf8c0041c95fd4d12c2ce786b8d39

---
+++ components/LightTablePro/index.tsx
@@ -0,0 +1,142 @@
+import { Card } from 'antd';
+import type { FormInstance, FormProps } from 'antd';
+import React from 'react';
+import styled from 'styled-components';
+import type { ActionRef, LightColumnProps, LightTableProps } from '../LightTable';
+import LightTable from '../LightTable';
+import type { SearchColumnsProps } from '../Search';
+import Search from '../Search';
+import type { QueryKey, QueryFunction, UseQueryOptions } from 'react-query';
+import useWrap from './hook/useWrap';
+
+const Space = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  overflow: visible;
+`;
+
+const SpaceBetween = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  gap: 16px;
+  overflow: visible;
+  justify-content: space-between;
+
+  background-color: #fff;
+`;
+
+type RequestParameters<RecordType> = Parameters<
+  NonNullable<LightTableProps<RecordType>['onChange']>
+>;
+
+export interface LightTableProProps<RecordType>
+  extends Omit<LightTableProps<RecordType>, 'columns'> {
+  formRef?: React.MutableRefObject<FormInstance | undefined>;
+  formProps?: FormProps;
+  columns?: LightTableProColumnProps<RecordType>[];
+  manualRequest?: boolean;
+  queryKey?: QueryKey;
+  queryOptions?: Omit<
+    UseQueryOptions<
+      | {
+          data: RecordType[];
+          success: boolean;
+          total: number;
+          page: number;
+        }
+      | undefined
+    >,
+    'queryKey' | 'queryFn'
+  >;
+  request?: (
+    params?: RecordType,
+    pagination?: RequestParameters<RecordType>[0],
+    sorter?: RequestParameters<RecordType>[2],
+    filters?: RequestParameters<RecordType>[1],
+    extra?: Parameters<QueryFunction>,
+  ) => Promise<{
+    data: RecordType[];
+    success: boolean;
+    total: number;
+    page: number;
+  }>;
+  actionRef?: React.MutableRefObject<ActionRef | undefined>;
+  headerTitle?: React.ReactNode;
+  toolBarRender?: React.ReactNode;
+}
+
+export interface LightTableProColumnProps<RecordType>
+  extends LightColumnProps<RecordType>,
+    SearchColumnsProps<RecordType> {}
+
+export default function LightTablePro<RecordType extends Record<any, any> = any>({
+  columns,
+  formRef,
+  actionRef,
+  formProps,
+  queryKey,
+  queryOptions,
+  request,
+  manualRequest,
+  pagination,
+  headerTitle,
+  toolBarRender,
+  children,
+  ...props
+}: LightTableProProps<RecordType>) {
+  const {
+    formHandler,
+    tableHandler,
+    actionRef: innerActionRef,
+    formRef: innerFormRef,
+  } = useWrap({
+    queryKey,
+    queryOptions,
+    request,
+    manualRequest,
+    pagination,
+  });
+
+  if (formRef?.current) {
+    formRef.current = innerFormRef.current;
+  }
+
+  if (actionRef?.current) {
+    actionRef.current = innerActionRef.current;
+  }
+
+  return (
+    <Space>
+      <Search
+        columns={columns}
+        formProps={{
+          ...formHandler,
+          ...formProps,
+        }}
+      />
+      {children}
+      <Card>
+        <Space>
+          <SpaceBetween>
+            <div>{headerTitle}</div>
+            <div>{toolBarRender}</div>
+          </SpaceBetween>
+
+          <LightTable<RecordType>
+            size="small"
+            sticky
+            columnEmptyText="-"
+            bordered
+            columns={columns}
+            {...tableHandler}
+            {...props}
+          />
+        </Space>
+      </Card>
+    </Space>
+  );
+}
