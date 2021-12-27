573bf47bb97a4611fa2cbd4089fc34d94db5d77b
fuRan <NgeKaworu@gmail.com>
2021-12-27 21:19:39 +0800

    feat: dyn

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 export const qiankun = {
   // 应用加载之前
-  async bootstrap(props: any) {},
+  async bootstrap(props: any) {
+    if (window.__POWERED_BY_QIANKUN__) {
+      __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+    }
+  },
   // 应用 render 之前触发
   async mount(props: any) {},
   // 应用卸载之后触发
@@ -8,3 +12,11 @@ export const qiankun = {
   // loadMicroApp 方式加载微应用时生效
   async update(props: any) {},
 };
+
+declare var __webpack_public_path__: string;
+declare global {
+  interface Window {
+    __POWERED_BY_QIANKUN__: boolean;
+    __INJECTED_PUBLIC_PATH_BY_QIANKUN__: string;
+  }
+}
